83e1421f25056e01c9b3851832da6f924e4d764b
MatteoGunetto <Matteogunetto@tiscali.it>
2023-06-05 16:28:25 +0200

    add single card example

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <script >
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
-import AppFooter from './components/AppFooter.vue'
+import AppFooter from './components/AppFooter.vue';
+import axios from 'axios'
   export default{
     name: 'App',
     components:{
       AppHeader,
       AppMain,
       AppFooter
+    },
+    created() {
+      axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+      .then((response) => {
+        
+      }
+      );
     }
   }
 </script>
-
 <template>
   <AppHeader/>
   <AppMain />
   <AppFooter />
-
+  
 </template>
 
 <style lang="scss">
- @import '../src/styles/main.scss';
+  @import '../src/styles/main.scss';
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,8 +6,39 @@
 
 <template>
 
+    <main class="vh-100 py-4">
+
+        <div class="container">
+            <div class="mb-3">
+                <select name="archetype" id="archetype">
+                    <option value="alien">Alien</option>
+                </select>
+            </div>
+            <div class="my-card-container d-flex flex-wrap justify-content-around align-items-start py-3 px-4">
+                <div class="single-card text-center p-3">
+                    <img src="https://m.media-amazon.com/images/I/51n9U3qo-iL._AC_.jpg" alt="">
+                    <h5 class="text-white text-uppercase">nome carta</h5>
+                    <span> race carta</span>
+                </div>
+            </div>
+        </div>
+
+    </main>
 
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/main.scss' as *;
+    main{
+        background-color: $my-primary-color;
+    }
+    div.my-card-container{
+        background-color: white;
+    }
+    div.single-card{
+        background-color: $my-primary-color;
+        img{
+            height: 400px;
+        }
+    }
 </style>
